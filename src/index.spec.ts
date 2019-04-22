50ee1f50271a05dbe1f229290c9de0636ba5ec13
dab00 <da440dil@gmail.com>
2019-04-22 15:17:58 +0300

    add retries

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,13 +5,17 @@ import {
   Counter,
   Storage,
   ErrInvalidTTL,
-  ErrInvalidLimit
+  ErrInvalidLimit,
+  ErrInvalidRetryCount,
+  ErrInvalidRetryDelay
 } from '.'
 
 const key = 'key'
 const value = -1
 const ttl = 1000
 const limit = 5
+const retryCount = 2
+const retryDelay = 20
 const prefix = 'count#'
 
 describe('Counter constructor', () => {
@@ -46,6 +50,29 @@ describe('Counter constructor', () => {
       new Counter(storage, { ttl, limit: 4.2, prefix })
     }, new Error(ErrInvalidLimit))
   })
+  it('should throw if retryCount is less than zero', () => {
+    assert.throws(() => {
+      new Counter(storage, { ttl, retryCount: -1, retryDelay, prefix })
+    }, new Error(ErrInvalidRetryCount))
+  })
+
+  it('should throw if retryCount is not integer', () => {
+    assert.throws(() => {
+      new Counter(storage, { ttl, retryCount: 4.2, retryDelay, prefix })
+    }, new Error(ErrInvalidRetryCount))
+  })
+
+  it('should throw if retryDelay is less than zero', () => {
+    assert.throws(() => {
+      new Counter(storage, { ttl, retryCount, retryDelay: -1, prefix })
+    }, new Error(ErrInvalidRetryDelay))
+  })
+
+  it('should throw if retryDelay is not integer', () => {
+    assert.throws(() => {
+      new Counter(storage, { ttl, retryCount, retryDelay: 4.2, prefix })
+    }, new Error(ErrInvalidRetryDelay))
+  })
 })
 
 describe('Counter', () => {
@@ -53,11 +80,20 @@ describe('Counter', () => {
   const incr = sinon.stub().resolves(value)
   storage.incr = incr
 
-  const counter = new Counter(storage, { ttl, limit, prefix })
+  const counter = new Counter(storage, { ttl, limit, retryCount, retryDelay, prefix })
 
   it('should count', async () => {
     const v = await counter.count(key)
     assert(v === value)
     assert(incr.calledOnceWithExactly(prefix + key, limit, ttl))
+  })
+
+  it('should retry count', async () => {
+    const incr = sinon.stub().resolves(1)
+    storage.incr = incr
+
+    const v = await counter.count(key)
+    assert(v === 1)
+    assert(incr.callCount === retryCount + 1)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,12 @@ export interface Storage {
 export const ErrInvalidTTL = 'ttl must be an integer greater than zero'
 /** ErrInvalidLimit is the error message returned when Counter constructor receives invalid value of limit. */
 export const ErrInvalidLimit = 'limit must be an integer greater than zero'
+/** ErrInvalidRetryCount is the error message returned when Counter constructor receives invalid value of retryCount. */
+export const ErrInvalidRetryCount = 'retryCount must be an integer greater than or equal to zero'
+/** ErrInvalidRetryDelay is the error message returned when Counter constructor receives invalid value of retryDelay. */
+export const ErrInvalidRetryDelay = 'retryDelay must be an integer greater than or equal to zero'
+/** ErrInvalidRetryJitter is the error message returned when Counter constructor receives invalid value of retryJitter. */
+export const ErrInvalidRetryJitter = 'retryJitter must be an integer greater than or equal to zero'
 
 /**
  * Counter implements distributed rate limiting.
@@ -22,12 +28,28 @@ export class Counter {
   private _storage: Storage;
   private _ttl: number;
   private _limit: number;
+  private _retryCount: number;
+  private _retryDelay: number;
+  private _retryJitter: number;
   private _prefix: string;
-  constructor(storage: Storage, { ttl, limit = 1, prefix = '' }: {
+  constructor(storage: Storage, { ttl, limit = 1, retryCount = 0, retryDelay = 0, retryJitter = 0, prefix = '' }: {
     /** TTL of key in milliseconds (must be greater than 0). */
     ttl: number;
     /** Maximum key value (must be greater than 0, by default equals 1). */
     limit?: number;
+    /** Maximum number of retries if key value limit is reached 
+     * (must be greater than or equal to 0, by default equals 0).
+     */
+    retryCount?: number;
+    /** Delay in milliseconds between retries if key value limit is reached 
+     * (must be greater than or equal to 0, by default equals 0).
+     */
+    retryDelay?: number;
+    /** Maximum time in milliseconds randomly added to delays between retries 
+     * to improve performance under high contention 
+     * (must be greater than or equal to 0, by default equals 0).
+     */
+    retryJitter?: number;
     /** Prefix of a key. */
     prefix?: string;
   }) {
@@ -37,9 +59,21 @@ export class Counter {
     if (!(Number.isSafeInteger(limit) && limit > 0)) {
       throw new Error(ErrInvalidLimit)
     }
+    if (!(Number.isSafeInteger(retryCount) && retryCount >= 0)) {
+      throw new Error(ErrInvalidRetryCount)
+    }
+    if (!(Number.isSafeInteger(retryDelay) && retryDelay >= 0)) {
+      throw new Error(ErrInvalidRetryDelay)
+    }
+    if (!(Number.isSafeInteger(retryJitter) && retryJitter >= 0)) {
+      throw new Error(ErrInvalidRetryJitter)
+    }
     this._storage = storage
     this._ttl = ttl
     this._limit = limit
+    this._retryCount = retryCount
+    this._retryDelay = retryDelay
+    this._retryJitter = retryJitter
     this._prefix = prefix
   }
   /**
@@ -48,6 +82,24 @@ export class Counter {
    * returns ttl in milliseconds if key value greater than limit.
    */
   count(key: string): Promise<number> {
-    return this._storage.incr(this._prefix + key, this._limit, this._ttl)
+    return this._count(this._prefix + key, this._retryCount)
   }
+  private async _count(key: string, counter: number): Promise<number> {
+    const v = await this._storage.incr(key, this._limit, this._ttl)
+    if (v === -1) {
+      return v
+    }
+    if (counter <= 0) {
+      return v
+    }
+    counter--
+    await sleep(Math.max(0, this._retryDelay + Math.floor((Math.random() * 2 - 1) * this._retryJitter)))
+    return this._count(key, counter)
+  }
+}
+
+function sleep(time: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time)
+  })
 }
