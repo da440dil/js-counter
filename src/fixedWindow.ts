e7c4264c338dd954c1f52f9dd4a44690db192e8d
da440dil <da440dil@gmail.com>
2021-10-17 19:44:06 +0300

    move lua scripts into package root

--- src/fixedWindow.ts
+++ src/fixedWindow.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs';
-import { join } from 'path';
+import { resolve } from 'path';
 import { Counter, ICounter, WindowParams } from './Counter';
 
 let src: string;
@@ -7,7 +7,7 @@ let src: string;
 /** Creates new counter which implements distributed rate limiting using fixed window algorithm. */
 export function fixedWindow({ client, size, limit }: WindowParams): ICounter {
 	if (!src) {
-		src = readFileSync(join(__dirname, 'fixedwindow.lua')).toString();
+		src = readFileSync(resolve(__dirname, '../fixedwindow.lua')).toString();
 	}
 	return new Counter({ client, size, limit, src });
 }
--- src/slidingWindow.ts
+++ src/slidingWindow.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs';
-import { join } from 'path';
+import { resolve } from 'path';
 import { Counter, ICounter, WindowParams } from './Counter';
 
 let src: string;
@@ -7,7 +7,7 @@ let src: string;
 /** Creates new counter which implements distributed rate limiting using sliding window algorithm. */
 export function slidingWindow({ client, size, limit }: WindowParams): ICounter {
 	if (!src) {
-		src = readFileSync(join(__dirname, 'slidingwindow.lua')).toString();
+		src = readFileSync(resolve(__dirname, '../slidingwindow.lua')).toString();
 	}
 	return new Counter({ client, size, limit, src });
 }
